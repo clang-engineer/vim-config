97dcce61ac6556e5ef29aa44d3866e094c116e69
yzero <yorez@naver.com>
2021-11-17 11:17:50 +0900

    modify: react skeleton

--- templates/skeleton.tsx
+++ templates/skeleton.tsx
@@ -1,6 +1,23 @@
-import * as React from "react"
-export default () => {
-  return (
-    <div></div>
-  )
+import React from 'react';
+import { connect } from 'react-redux';
+import { IRootState } from "app/shared/reducers";
+
+export interface ISkeleton extends StateProps, DispatchProps {
+
 }
+
+export const Skeleton = (props: ISkeleton) => {
+
+  return (<div>test</div>);
+
+};
+
+
+const mapStateToProps = ({}: IRootState) => ({});
+
+const mapDispatchToProps = {};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Skeleton);
